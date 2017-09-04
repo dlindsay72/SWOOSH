6a3b0816a9efd648598d7fba9c6bbd4d68d80c1a
Dan Lindsay <dlindsay72@hotmail.com>
2017-09-04 14:47:39 -0400

    added LeagueVC and SkillVC, some refactoring

---
+++ Swooosh/Controller/LeagueVC.swift
@@ -0,0 +1,19 @@
+//
+//  LeagueVC.swift
+//  Swooosh
+//
+//  Created by Dan Lindsay on 2017-09-04.
+//  Copyright © 2017 Dan Lindsay. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ Swooosh/Controller/SkillVC.swift
@@ -0,0 +1,19 @@
+//
+//  SkillVC.swift
+//  Swooosh
+//
+//  Created by Dan Lindsay on 2017-09-04.
+//  Copyright © 2017 Dan Lindsay. All rights reserved.
+//
+
+import UIKit
+
+class SkillVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
