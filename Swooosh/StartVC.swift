0e69c116190906a5d3563d6a5b122c5780bcc4ba
Dan Lindsay <dlindsay72@hotmail.com>
2017-09-04 14:15:22 -0400

    unwind segue from LeagueVC to StartVC done

--- Swooosh/StartVC.swift
+++ Swooosh/StartVC.swift
@@ -15,16 +15,11 @@ class StartVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+        
     }
-
 
 }
 
