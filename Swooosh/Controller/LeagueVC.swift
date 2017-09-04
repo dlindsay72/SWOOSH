7fd0ae5ca384c3b58781aface2591c9c4ec6c1f1
Dan Lindsay <dlindsay72@hotmail.com>
2017-09-04 16:27:00 -0400

    Player struct created, logic for LeagueVC actions done

--- Swooosh/Controller/LeagueVC.swift
+++ Swooosh/Controller/LeagueVC.swift
@@ -9,15 +9,46 @@
 import UIKit
 
 class LeagueVC: UIViewController {
-
+    
+    @IBOutlet weak var nextBtn: BorderButton!
+    @IBOutlet weak var mensBtn: BorderButton!
+    @IBOutlet weak var womensBtn: BorderButton!
+    @IBOutlet weak var coedBtn: BorderButton!
+    
+    //MARK: - Properties
+    var player: Player!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        player = Player()
     }
+    
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
+    
+    //MARK: - IBActions
     
     @IBAction func nextBtnPressed(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
+        
     }
+    
+    @IBAction func mensBtnTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    
+    @IBAction func womensBtnTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+    }
+    
+    @IBAction func coedBtnTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+        
+    }
+    
+    
     
 }
---
+++ Swooosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  Swooosh
+//
+//  Created by Dan Lindsay on 2017-09-04.
+//  Copyright © 2017 Dan Lindsay. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+}
